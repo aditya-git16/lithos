00e36d8099b5fb56bc88dbe53cef52fee94852c2
Aditya <es1221211@iitd.ac.in>
2026-02-05 01:41:32 +0530

    set mutable mmap struct and methods

--- crates/lithos/lithos-mmap/src/lib.rs
+++ crates/lithos/lithos-mmap/src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use memmap2::{Mmap, MmapMut};
+use std::{
+    fs::{File, OpenOptions},
+    io,
+    path::Path,
+};
+
+pub struct MmapFileMut {
+    _file: File,
+    mmap: MmapMut,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct MmapFile {
+    _file: File,
+    mmap: Mmap,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl MmapFileMut {
+    /// Create a new file to `size_bytes` and map it read-write
+    pub fn create_rw<P: AsRef<Path>>(path: P, size_bytes: u64) -> io::Result<Self> {
+        let file = OpenOptions::new()
+            .create(true)
+            .read(true)
+            .write(true)
+            .truncate(true)
+            .open(path)?;
+        file.set_len(size_bytes)?;
+
+        let mmap = unsafe { MmapMut::map_mut(&file)? };
+        Ok(Self { _file: file, mmap })
+    }
+
+    /// Open an existing file and map it to read and write
+    pub fn open_rw<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        let file = OpenOptions::new().read(true).write(true).open(path)?;
+
+        let mmap = unsafe { MmapMut::map_mut(&file)? };
+
+        Ok(Self { _file: file, mmap })
+    }
+
+    /// Return raw pointer to start of memory mapped file data
+    #[inline]
+    pub fn as_mut_ptr(&mut self) -> *mut u8 {
+        self.mmap.as_mut_ptr()
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.mmap.len()
     }
 }
