6ae1340b9183fc7df702ae6b7c0d5036d0f8e2e6
Aditya <es1221211@iitd.ac.in>
2026-02-10 04:15:51 +0530

    setup config crate for onyx

---
+++ crates/onyx/onyx-config/src/config.rs
@@ -0,0 +1,39 @@
+use std::{error::Error, path::Path};
+
+use serde::Deserialize;
+
+// our aim is to load values from the config toml file
+// and then set the values to the config struct and initialise
+// the engine using these config values
+
+#[derive(Deserialize, Debug)]
+pub struct OnyxConfig {
+    #[serde(default = "defaults::shm_file_path")]
+    pub shm_file_path: String,
+    #[serde(default = "defaults::log_level")]
+    pub log_level: String,
+}
+
+mod defaults {
+    pub fn shm_file_path() -> String {
+        "/tmp/lithos_md_bus".into()
+    }
+
+    pub fn log_level() -> String {
+        "info".into()
+    }
+}
+
+impl OnyxConfig {
+    // this function takes path to the config as the input
+    pub fn load(
+        path: impl AsRef<Path>,
+    ) -> Result<Self, Box<dyn Error + Send + Sync + 'static + Sync>> {
+        // first we load the contents on the toml file and then we read it ?
+        // so in the toml crate we read the toml document as a str so we first need to convert
+        // the toml file contents into string
+        let toml_to_str = std::fs::read_to_string(path)?;
+        let onyx_config: OnyxConfig = toml::from_str(&toml_to_str)?;
+        Ok(onyx_config)
+    }
+}
