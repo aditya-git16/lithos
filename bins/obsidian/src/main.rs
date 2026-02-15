da84304f8e89b73240f3d1be5d03977376bd1e37
Aditya <es1221211@iitd.ac.in>
2026-02-16 00:48:19 +0530

    remve String allocations for dto , use lifetime inferenced struct

--- bins/obsidian/src/main.rs
+++ bins/obsidian/src/main.rs
@@ -11,15 +11,15 @@ fn now_ns() -> u64 {
 }
 
 #[derive(Debug, Deserialize)]
-pub struct BinanceDto {
+pub struct BinanceDto <'a> {
     pub u: u64,    // order book updateId
-    pub s: String, // symbol
-    pub b: String, // best bid price
+    pub s: &'a str, // symbol
+    pub b: &'a str, // best bid price
     #[serde(rename = "B")]
-    pub b_qty: String, // best bid qty
-    pub a: String, // best ask price
+    pub b_qty: &'a str, // best bid qty
+    pub a: &'a str, // best ask price
     #[serde(rename = "A")]
-    pub a_qty: String, // best ask qty
+    pub a_qty: &'a str, // best ask qty
 }
 
 fn main() {
