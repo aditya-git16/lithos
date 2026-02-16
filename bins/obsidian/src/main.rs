7283db038533bfacc3b44770909fc06871984fae
Aditya <es1221211@iitd.ac.in>
2026-02-16 18:49:22 +0530

    add config setup for obsidian

--- bins/obsidian/src/main.rs
+++ bins/obsidian/src/main.rs
@@ -1,5 +1,6 @@
 use lithos_events::{SymbolId, TopOfBook};
 use lithos_icc::{BroadcastWriter, RingConfig};
+use obsidian_config::config::ObsidianConfig;
 use obsidian_core::dto::BinanceDto;
 use obsidian_util::floating_parse::{parse_px_2dp, parse_qty_3dp};
 use obsidian_util::timestamp::now_ns;
@@ -7,9 +8,9 @@ use tracing::info;
 use tracing_subscriber::EnvFilter;
 use tungstenite::{Message, connect};
 
-fn main() {
-    let path = "/tmp/lithos_md_bus";
-    let capacity = 1 << 16;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config_path = "/Users/adityaanand/dev/lithos/config/obsidian/config.toml";
+    let config = ObsidianConfig::load(&config_path)?;
 
     tracing_subscriber::fmt()
         .with_env_filter(
@@ -17,13 +18,19 @@ fn main() {
         )
         .init();
 
-    let mut bus = BroadcastWriter::<TopOfBook>::create(path, RingConfig::new(capacity))
-        .expect("failed to create mmap ring");
+    let mut bus = BroadcastWriter::<TopOfBook>::create(
+        &config.shm_file_path,
+        RingConfig::new(config.capacity),
+    )
+    .expect("failed to create mmap ring");
 
-    info!("OBSIDIAN: publishing TopOfBook to {path} (cap={capacity})");
+    info!(
+        "OBSIDIAN: publishing TopOfBook to {path} (cap={capacity})",
+        path = &config.shm_file_path,
+        capacity = config.capacity
+    );
 
-    let (mut socket, _resposne) =
-        connect("wss://stream.binance.com:9443/ws/btcusdt@bookTicker").expect("failed to connect");
+    let (mut socket, _resposne) = connect(&config.binance_ws_url).expect("failed to connect");
     info!("Connected to Binance Websocket Server");
 
     loop {
@@ -52,4 +59,6 @@ fn main() {
             _ => {}
         }
     }
+
+    Ok(())
 }
---
+++ crates/obsidian/obsidian-config/src/config.rs
@@ -0,0 +1,66 @@
+use std::path::Path;
+
+use serde::Deserialize;
+
+// our aim is to load values from the config toml file
+// and then set the values to the config struct and initialise
+// the engine using these config values
+
+#[derive(Deserialize, Debug)]
+pub struct ObsidianConfig {
+    #[serde(default = "defaults::shm_file_path")]
+    pub shm_file_path: String,
+    #[serde(default = "defaults::log_level")]
+    pub log_level: String,
+    #[serde(default = "defaults::binance_ws_url")]
+    pub binance_ws_url: String,
+    #[serde(default = "defaults::capacity")]
+    pub capacity: usize,
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ConfigError {
+    #[error("failed to read '{path}'")]
+    Read {
+        path: String,
+        #[source]
+        source: std::io::Error,
+    },
+
+    #[error("failed to parse config")]
+    Parse(#[from] toml::de::Error),
+}
+
+mod defaults {
+    pub fn shm_file_path() -> String {
+        "/tmp/lithos_md_bus".into()
+    }
+
+    pub fn log_level() -> String {
+        "info".into()
+    }
+
+    pub fn binance_ws_url() -> String {
+        "wss://stream.binance.com:9443/ws/btcusdt@bookTicker".into()
+    }
+
+    pub fn capacity() -> usize {
+        return 1 << 16; // 65536
+    }
+}
+
+impl ObsidianConfig {
+    // this function takes path to the config as the input
+    // result type should be a config error enum
+    pub fn load(path: impl AsRef<Path> + ToString) -> Result<Self, ConfigError> {
+        // first we load the contents on the toml file and then we read it ?
+        // so in the toml crate we read the toml document as a str so we first need to convert
+        // the toml file contents into string
+        let toml_to_str = std::fs::read_to_string(&path).map_err(|source| ConfigError::Read {
+            path: path.to_string(),
+            source,
+        })?;
+        let onyx_config: ObsidianConfig = toml::from_str(&toml_to_str)?;
+        Ok(onyx_config)
+    }
+}
