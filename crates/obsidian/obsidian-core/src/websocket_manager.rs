084c370ff4b1439b2b545d2d7e7295b6630cffea
Aditya <es1221211@iitd.ac.in>
2026-02-17 00:50:24 +0530

    setup obsidian engine

--- crates/obsidian/obsidian-core/src/websocket_manager.rs
+++ crates/obsidian/obsidian-core/src/websocket_manager.rs
@@ -1,1 +1,18 @@
-pub struct WebsocketManager;
+pub const MAX_SYMBOLS: usize = 256;
+
+#[derive(Default)]
+pub struct WebsocketSymbolState {
+    pub book_tikcer: String,
+}
+
+pub struct WebsocketManager {
+    pub websocket_connections: [WebsocketSymbolState; MAX_SYMBOLS],
+}
+
+impl WebsocketManager {
+    pub fn new() -> Self {
+        Self {
+            websocket_connections: std::array::from_fn(|_| WebsocketSymbolState::default()),
+        }
+    }
+}
--- crates/obsidian/obsidian-engine/src/lib.rs
+++ crates/obsidian/obsidian-engine/src/lib.rs
@@ -1,8 +1,34 @@
 use lithos_events::Event;
-use lithos_icc::BroadcastWriter;
+use lithos_icc::{BroadcastWriter, RingConfig};
+use obsidian_config::config::ObsidianConfig;
 use obsidian_core::websocket_manager::WebsocketManager;
+use std::net::TcpStream;
+use std::path::Path;
+use tungstenite::stream::MaybeTlsStream;
+use tungstenite::{WebSocket, connect};
 
-pub struct ObsidianEngine{
+pub type WebsocketStream = WebSocket<MaybeTlsStream<TcpStream>>;
+
+pub struct ObsidianEngine {
+    pub ws_stream: WebsocketStream,
     pub ws_manager: WebsocketManager,
-    pub writer: BroadcastWriter<Event>
+    pub writer: BroadcastWriter<Event>,
+}
+
+impl ObsidianEngine {
+    pub fn new<P: AsRef<Path>>(
+        path: P,
+        capacity: RingConfig,
+        config: &ObsidianConfig,
+    ) -> std::io::Result<Self> {
+        let (socket, _resposne) = connect(&config.binance_ws_url).expect("failed to connect");
+        // error handling for this function also needed
+        let ws_manager = WebsocketManager::new();
+        let writer = BroadcastWriter::<Event>::create(path, capacity)?;
+        Ok(ObsidianEngine {
+            ws_stream: socket,
+            ws_manager: ws_manager,
+            writer: writer,
+        })
+    }
 }
