4d64e07fd395a60f06dc3c29c68efbace26d726d
Aditya <es1221211@iitd.ac.in>
2026-02-17 09:07:41 +0530

    optimise timestamping

--- crates/obsidian/obsidian-util/src/timestamp.rs
+++ crates/obsidian/obsidian-util/src/timestamp.rs
@@ -1,7 +1,7 @@
+/// Returns the current monotonic time in nanoseconds.
 #[inline(always)]
 pub fn now_ns() -> u64 {
-    let t = std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .unwrap();
-    t.as_nanos() as u64
+    let mut ts: libc::timespec = unsafe { core::mem::zeroed() };
+    unsafe { libc::clock_gettime(libc::CLOCK_MONOTONIC, &mut ts) };
+    ts.tv_sec as u64 * 1_000_000_000 + ts.tv_nsec as u64
 }
